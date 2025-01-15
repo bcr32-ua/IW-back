f9dd5f6d5d642cf4b8af1841a28ea7d72c3615c6
pathbt <elg60@alu.ua.es>
2025-01-15 15:18:35 +0100

    Listado de 'Mis reservas' inicial

--- src/main/java/com/IW/back/controller/BookingController.java
+++ src/main/java/com/IW/back/controller/BookingController.java
@@ -5,7 +5,6 @@ import com.IW.back.repository.SeasonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 import com.IW.back.model.Booking;
 import com.IW.back.repository.BookingRepository;
@@ -21,28 +20,32 @@ public class BookingController {
     private SeasonRepository seasonRepository;
 
     @GetMapping
-    public List<Booking> getAllBookings(){
+    public List<Booking> getAllBookings() {
         return bookingRepository.findAll();
     }
 
     @PostMapping
-    public Booking createBooking(@RequestBody Booking booking){
+    public Booking createBooking(@RequestBody Booking booking) {
         return bookingRepository.save(booking);
     }
 
     @PutMapping
-    public Booking updateBooking(@RequestBody Booking booking){
+    public Booking updateBooking(@RequestBody Booking booking) {
         return bookingRepository.save(booking);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBooking(@PathVariable Long id){
+    public void deleteBooking(@PathVariable Long id) {
         bookingRepository.deleteById(id);
     }
 
     @GetMapping("/{id}")
-    public Booking getBookingById(@PathVariable Long id){
+    public Booking getBookingById(@PathVariable Long id) {
         return bookingRepository.findById(id).orElse(null);
     }
-}
 
+    @GetMapping("/user/{userId}")
+    public List<Booking> getBookingsByUserId(@PathVariable Long userId) {
+        return bookingRepository.findByUserId(userId);
+    }
+}
